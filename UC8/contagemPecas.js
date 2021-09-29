c5b3a191900a0b1d1e768cf05a47507fb6c4c329
SabrinaMalafaia <brina.sxm@hotmail.com>
2021-09-28 21:14:09 -0300

    Adicionando o arquivo contagemPecas.js da atividade do Encontro Presencial 1 do módulo UC8.

---
+++ UC8/contagemPecas.js
@@ -0,0 +1,5 @@
+// Autora: Sabrina Malafaia.
+// Algoritmo sistema de contagem de peças:
+let listaPecas = ["Peça A","Peça B","Peça C"];
+let quantidadePecas = listaPecas.length;
+console.log("Total de peças: "+ quantidadePecas);
